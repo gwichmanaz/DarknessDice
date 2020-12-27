597da6e5f3605bcd62786756ca0cfb40a0085dba
Glenn R. Wichman <gwichman@zynga.com>
2020-12-26 21:01:49 -0800

    RNGs were all getting the same seed

--- public/js/RNG.js
+++ public/js/RNG.js
@@ -1,15 +1,21 @@
+var salt = 0;
+
+var saltSeed = () => {
+	return Date.now() + ++salt;
+}
+
 export class RNG {
 	constructor(seed) {
-		seed = seed || Date.now();
+		seed = seed || saltSeed();
 		this.seed = Math.floor(Math.abs(seed));
 		this.uses = 0;
 	}
 	next() {
 		this.uses++;
-		this.seed = Math.abs(Math.imul(48271, this.seed) | 0 % 2147483647);
+		this.seed = Math.imul(48271, this.seed) | 0 % 2147483647;
 		return this.seed;
 	}
 	minmax(min, max) {
-		return min + (this.next() % (1 + max - min));
+		return min + (Math.abs(this.next()) % (1 + max - min));
 	}
 }
